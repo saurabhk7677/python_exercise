891faf038039f6aa157aa8df14fcf87f6701caeb
saurabh <saurabhkadam1105@gmail.com>
2022-09-08 14:27:09 +0530

    add all  files

---
+++ exersize2.py
@@ -0,0 +1,9 @@
+# A cooment, this is so you can read your program later.
+# Anything after the # is igonered by python.
+
+print("I could have like this.") # and the comment after is ignored
+
+# you can also use a comment to "Disable" or comment out code:
+#print("This won't run.")
+
+print("This will run.")
