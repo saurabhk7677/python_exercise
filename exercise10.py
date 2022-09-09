50dd7b71b815982c9236720ccacbda516e11797a
saurabh <saurabhkadam1105@gmail.com>
2022-09-09 10:01:03 +0530

    add new print line in exercise10.py

--- exercise10.py
+++ exercise10.py
@@ -1,6 +1,7 @@
 tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
+print("I am 6'2\" tall")   # double quote in duoble quote using \
 
 fat_cat = """
 I'll do a list:
