38e47e702b1df1fded9a097f346374b315509cab
saurabh <saurabhkadam1105@gmail.com>
2022-09-13 23:20:02 +0530

    add exercise 28 to 33 and one modify ex26

--- exercise26.py
+++ exercise26.py
@@ -79,10 +79,10 @@ dogs = 15
 if people < cats:
     print("Too many cats! The world is doomed!")
 
-if people < cats:
+if people > cats:
     print("Not many cats! The world is saved!")
 
-if people > dogs:
+if people < dogs:
     print("The world is drooled on!")
 
 if people > dogs:
