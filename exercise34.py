983dbcf04f940454ab8aa7c5b88f3e1f233bcab7
saurabh <saurabhkadam1105@gmail.com>
2022-09-15 09:23:47 +0530

    add exercise 34

---
+++ exercise34.py
@@ -0,0 +1,25 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# The animal at 1
+print(animals[0])
+
+# The thrid (3rd) animal
+print(animals[2])
+
+# The frist (1st) animal
+print(animals[0])
+
+# The animal at 3
+print(animals[2])
+
+# The fifth (5th) animal
+print(animals[4])
+
+# The animal at 2
+print(animals[1])
+
+# The sixth (6th) animal
+print(animals[5])
+
+# The animal at 4
+print(animals[3])
